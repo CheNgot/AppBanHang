ddbb692e15ef29a7bf509f9fb5ad26d8166598c7
Quoc Thai <Quoc Thai@DESKTOP-PT4NTVI>
2019-03-21 22:29:26 +0700

    ánh xạ các thứ để bắt sự kiệm

--- app/src/main/java/com/example/quocthai/appbanhang/MainActivity.java
+++ app/src/main/java/com/example/quocthai/appbanhang/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.quocthai.appbanhang;
 
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ListView;
+import android.widget.Toolbar;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerView;
+    NavigationView navigationView;
+    ListView listViewmanhinhchinh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void AnhXa()
+    {
+        toolbar =  findViewById(R.id.toolbarmanhinhchinh);
+        viewFlipper = findViewById(R.id.viewflipper);
+        recyclerView = findViewById(R.id.recyclervew);
+        navigationView = findViewById(R.id.navigationview);
+        listViewmanhinhchinh = findViewById(R.id.listviewmanhinhchinh);
+
+
     }
 }
