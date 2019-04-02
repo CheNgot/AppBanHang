4ebc994251b4bdba97d8287bb710783eae683638
Quoc Thai <Quoc Thai@DESKTOP-PT4NTVI>
2019-04-02 23:07:52 +0700

    thử bắt sự kiện click trên từng sản phẩm

--- app/src/main/java/com/example/quocthai/appbanhang/adapter/SanphamAdapter.java
+++ app/src/main/java/com/example/quocthai/appbanhang/adapter/SanphamAdapter.java
@@ -1,6 +1,7 @@
 package com.example.quocthai.appbanhang.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,7 +11,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.quocthai.appbanhang.R;
+import com.example.quocthai.appbanhang.activity.ChiTietSanPham;
 import com.example.quocthai.appbanhang.model.Sanpham;
+import com.example.quocthai.appbanhang.ultil.CheckConnection;
 import com.squareup.picasso.Picasso;
 
 import java.text.DecimalFormat;
@@ -60,6 +63,17 @@ public class SanphamAdapter extends RecyclerView.Adapter<SanphamAdapter.ItemHold
             imghinhsanpham=itemView.findViewById(R.id.imageviewsanpham);
             txttensanpham=itemView.findViewById(R.id.textviewtensanpham);
             txtgiasanpham=itemView.findViewById(R.id.textviewgiasanpham);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context,ChiTietSanPham.class);
+                    intent.putExtra("thongtinsanpham",arraysanpham.get(getPosition()));
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    CheckConnection.ShowToast_short(context,arraysanpham.get(getPosition()).getTenSanPham());
+                    context.startActivity(intent);
+
+                }
+            });
         }
     }
 }
